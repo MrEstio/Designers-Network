0ded1357d5d718907e70d817ff087b2186c8544c
Deku <Deku@PREDATOR>
2022-09-24 19:44:10 +0300

    Добавьте файлы проекта.

---
+++ Designers Network/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string username;
+	string password;
+	bool success;
+	cout << "\tGame Designer's Network\n";
+	do
+	{
+		cout << "\nUsername:";
+		cin >> username;
+		cout << "Password:";
+		cin >> password;
+		if (username == "S.Meir" && password == "civilization")
+		{
+			cout << "\nHey, Sid!";
+			success = true;
+		}
+		else if (username == "S.Miyamoto" && password == "mariobros")
+		{
+			cout << "\nWhat's up. Shigeru?";
+			success = true;
+		}
+		else if (username == "W.Wrigh" && password == "thesims")
+		{
+			cout << "\nHow goes it.Will ? ";
+			success = true;
+		}
+		else if (username == "guest" && password == "guest")
+		{
+			cout << "\nWelcome. guest.";
+			success = true;
+		}
+		else
+		{
+			cout << "\nYour login failed.\n";
+			success = false;
+		}
+	} while (!success);
+	return 0;
+}
